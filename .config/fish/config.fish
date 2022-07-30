25d4901b8cb1da700c882526a48c5f2be5314443
jay <julius.rahaus@qciety.org>
2022-07-30 14:38:41 +0200

    make nvm optional

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,16 @@
 set -gx PATH /bin /usr/bin /usr/local/bin /usr/games /usr/sbin
-set -gx NVM_DIR $HOME/.nvm
 set -gx EDITOR /usr/bin/nvim
 set -gx TERM xterm-256color
 
-bass source $NVM_DIR/nvm.sh
+if test -e $HOME/.nvm
+  set -gx NVM_DIR $HOME/.nvm
+  bass source $NVM_DIR/nvm.sh
+end
 
 if status is-interactive
+  if not test -n "$TMUX"
+    tmux new-session -A -s MAIN
+  end
   fish_vi_key_bindings
   set -g theme_display_date no
   set -g theme_display_cmd_duration no
@@ -13,8 +18,6 @@ if status is-interactive
   set -g theme_display_ruby no
   set -g theme_show_exit_status no
   set -U fish_greeting "🐟"
-  # start tmux if not opened inside tmux
-  if not test -n "$TMUX"
-    tmux new-session -A -s MAIN
-  end
+  
+  alias dotfiles "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 end
