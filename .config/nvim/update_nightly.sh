90871bce78706358adb87e668b29d7c8f6499af3
jay <julius.rahaus@qciety.org>
2023-01-29 19:23:36 +0100

    working nightly update

--- .config/nvim/update_nightly.sh
+++ .config/nvim/update_nightly.sh
@@ -1,7 +1,32 @@
 #! /bin/sh
 
-wget -O ~/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-tar xzvf ~/nvim-linux64.tar.gz
-cp -r ~/nvim-linux64 /usr/share/.
-rm -rf ~/nvim-linux64
-rm -f ~/nvim-linux64.tar.gz
+
+if [ ! -w /opt ] || [ ! -w /usr/local/bin ]; then
+	echo "needs write acces to /opt and /usr/local/bin" && exit 1
+fi
+
+echo -n "- create tmp directory ..."
+tmpdir=$(mktemp -d)
+echo " OK"
+echo "- download nvim:"
+wget -P $tmpdir -q --show-progress https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+echo "- download checksum: "
+wget -P $tmpdir -q --show-progress https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz.sha256sum
+echo -n "- test checksum ... "
+sha256sum --check $tmpdir/nvim-linux64.tar.gz.sha256sum || exit 1
+echo -n "- unpack tar gz ... "
+tar --directory $tmpdir --totals -xzf $tmpdir/nvim-linux64.tar.gz
+if [ -d /opt/nvim ]; then
+	echo -n "- remove old installation ..."
+	rm -rf /opt/nvim
+  echo " OK"
+fi
+echo -n "- install files to /opt/nvim ..."
+cp -r $tmpdir/nvim-linux64 /opt/nvim
+echo " OK"
+echo -n "- link bin to /usr/local/bin/nvim ..."
+ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
+echo " OK"
+echo -n "- delete tmp directory ..."
+rm -rf $tmpdir
+echo " OK"
